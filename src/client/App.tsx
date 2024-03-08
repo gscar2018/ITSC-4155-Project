a036f29985c8c5ac0884b13fed5eea4e907fd89e
Jaylon Carrington <carringtonjaylon4@gmail.com>
2024-03-07 20:16:55 -0500

    inital commit

---
+++ src/client/App.tsx
@@ -0,0 +1,34 @@
+import "./App.css";
+
+import { useState } from "react";
+
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState("Data has not loaded yet");
+
+  /**
+   * Fetch data from the server and update the component state
+   * @returns A Promise that resolves when the data is fetched and set in the component state
+   */
+  const fetchData = (): Promise<void> => {
+    return fetch("/hello")
+      .then((response: Response) => response.text())
+      .then((data: string) => setData(data));
+  };
+
+  return (
+    <div>
+      <div>
+        <h1 className="text-primary font-bold">
+          Hello Vite + React + TypeScript
+        </h1>
+        <p>{data}</p>
+        <button onClick={() => fetchData()}>Fetch data from the server</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
