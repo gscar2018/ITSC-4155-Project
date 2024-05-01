ca9339f30045492ac1b89437fd71e80b868703ad
gscar2018 <gscardo@ymail.com>
2024-05-01 13:47:55 -0400

    Feat: imagineUpload
    Added a preview to the image being uploaded

--- src/client/components/ImageUpload.tsx
+++ src/client/components/ImageUpload.tsx
@@ -2,17 +2,21 @@ import type React from "react";
 import { useRef, useState } from "react";
 import { Icon } from "@iconify/react/dist/iconify.js";
 
+
 function ImageUpload() {
 	// Explicitly define the type of state as `File | null`
 	const [selectedFile, setSelectedFile] = useState<File | null>(null);
 	const fileInputRef = useRef<HTMLInputElement>(null);
 	const [fileName, setFileName] = useState<string | null>(null);
+	const [previewURL, setPreviewURL] = useState<string | null>(null); // Add previewURL state
+
 	// Handles file selection
 	const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
 		event.preventDefault();
 		event.stopPropagation();
 		event.dataTransfer.dropEffect = "copy";
 	};
+
 
 	const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
 		event.preventDefault();
@@ -22,22 +26,28 @@ function ImageUpload() {
 		setSelectedFile(file);
 		setFileName(file.name);
 
+		setPreviewURL(URL.createObjectURL(file)); // Set the previewURL
 		//set file input value to the dropped file
 		if (fileInputRef.current) {
 			fileInputRef.current.files = event.dataTransfer.files;
 		}
 	};
+
 	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		if (event.target.files && event.target.files.length > 0) {
-			setSelectedFile(event.target.files[0]);
-			setFileName(event.target.files[0].name);
-			console.log(event.target.files[0]);
+			const file = event.target.files[0];
+			setSelectedFile(file);
+			setFileName(file.name);
+			setPreviewURL(URL.createObjectURL(file)); // Set the previewURL
+			console.log(file);
 		} else {
 			setSelectedFile(null); // Ensure state is reset if no file is selected
 			setFileName(null);
+			setPreviewURL(null); // Reset the previewURL
 			console.log("No file selected");
 		}
 	};
+
 
 	return (
 		<div className="join join-vertical">
@@ -56,16 +66,20 @@ function ImageUpload() {
 					required
 				/>
 				<div className="flex flex-col items-center justify-center">
-          <Icon icon="mdi:upload" className="text-6xl" />
+					<Icon icon="mdi:upload" className="text-6xl" />
 					<p className="text-lg text-accent-content">
 						Click to upload or drag & Drop
 					</p>
 					{fileName && <p className="text-sm font-light italic">{fileName}</p>}
 					<p className="text-sm font-light italic">PNG JPG or SVG</p>
+					{previewURL && (
+						<img src={previewURL} alt="Preview" className="mt-4" />
+					)}
 				</div>
 			</div>
 		</div>
 	);
 }
+
 
 export default ImageUpload;
