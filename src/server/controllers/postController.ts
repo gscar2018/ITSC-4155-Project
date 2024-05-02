305fb06a752b2ca462e06332aec6c07b70bd71a8
Jaylon Carrington <carringtonjaylon4@gmail.com>
2024-05-01 23:39:35 -0400

    search by substr instead of exact match

--- src/server/controllers/postController.ts
+++ src/server/controllers/postController.ts
@@ -4,9 +4,13 @@ import { PostModel } from "../schemas/Post.ts";
 //searching posts by tag
 export const searchPostsByTags = async (req: Request, res: Response) => {
 	const { tags } = req.query;
-
+	if (!tags) {
+		return res.status(400).json({ error: "Tags must be provided" });
+	}
 	try {
-		const posts = await PostModel.find({ tags: { $in: tags } });
+		const tagsArr = Array.isArray(tags) ? tags : [tags];
+		const regexSearch = tagsArr.map((tag) => new RegExp(tag.toString(), "i"));
+		const posts = await PostModel.find({ tags: { $in: regexSearch } });
 		return res.json(posts);
 	} catch (error) {
 		console.error("Error searching posts:", error);
